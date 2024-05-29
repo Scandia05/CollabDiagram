fd2c04e77bee37297e9fdef101ad3e9931be4330
Sebastián Candia Cabezas <s.candia05@ufromail.cl>
2024-05-29 09:17:06 -0400

    Eliminación Archivo Server.js fuera de la carpeta Servidor

--- Servidor/server.js
+++ Servidor/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "*", // Permitir accesos desde cualquier origen
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });
