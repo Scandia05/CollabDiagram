8c68d467535fafb8a0d2a9aaf40742847ac4d91b
Sebastián Candia Cabezas <s.candia05@ufromail.cl>
2024-06-03 20:02:01 -0400

    Correcciones

--- Cliente/src/Components/Login.js
+++ Cliente/src/Components/Login.js
@@ -7,40 +7,40 @@ import './Auth.css';
 const Login = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
+    const [message, setMessage] = useState('');
     const navigate = useNavigate();
-    const { login } = useContext(AuthContext);
+    const { saveAuthData } = useContext(AuthContext);
 
-    const handleSubmit = async (e) => {
+    const handleLogin = async (e) => {
         e.preventDefault();
         try {
             const response = await axios.post('http://200.13.4.230:4000/login', { username, password });
-            login(response.data.token, username);
-            navigate('/main');
-        } catch (err) {
-            setError('Invalid credentials');
+            saveAuthData(response.data.token, username);
+            navigate('/menu'); // Redirigir a la página del menú
+        } catch (error) {
+            setMessage('Invalid credentials');
         }
     };
 
+    const goToRegister = () => {
+        navigate('/register');
+    };
+
     return (
-        <div className="login-container">
-            <form onSubmit={handleSubmit}>
+        <div className="auth-container">
+            <form className="auth-form" onSubmit={handleLogin}>
                 <h2>Login</h2>
-                <input
-                    type="text"
-                    placeholder="Username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-                <input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
+                <div>
+                    <label>Username:</label>
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Enter your username" />
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter your password" />
+                </div>
                 <button type="submit">Login</button>
-                {error && <p className="error">{error}</p>}
-                <button onClick={() => navigate('/register')}>¿Quiere registrarse?</button>
+                {message && <p className="message">{message}</p>}
+                <button type="button" className="switch-button" onClick={goToRegister}>¿Quiere registrarse?</button>
             </form>
         </div>
     );
